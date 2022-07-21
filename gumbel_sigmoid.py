f85cd837860a71fc5061a65fb11be28bcf85e318
Angelos Nalmpantis <73990700+AngelosNal@users.noreply.github.com>
2022-07-21 23:23:26 +0200

    Added gumbel_sigmoid

---
+++ gumbel_sigmoid.py
@@ -0,0 +1,43 @@
+import torch
+
+from torch import Tensor
+
+
+def gumbel_sigmoid(logits: Tensor, tau: float = 1, hard: bool = False, threshold: int = 0.5) -> Tensor:
+    """
+    Samples from the Gumbel-Sigmoid distribution and optionally discretizes.
+    The discretization converts the values greater than `threshold` to 1 and the rest to 0.
+    The code is adapted from the official PyTorch implementation of gumbel_softmax:
+    https://pytorch.org/docs/stable/_modules/torch/nn/functional.html#gumbel_softmax
+
+    Args:
+      logits: `[..., num_features]` unnormalized log probabilities
+      tau: non-negative scalar temperature
+      hard: if ``True``, the returned samples will be discretized,
+            but will be differentiated as if it is the soft sample in autograd
+     threshold: threshold for the discretization,
+                values greater than this will be set to 1 and the rest to 0
+
+    Returns:
+      Sampled tensor of same shape as `logits` from the Gumbel-Sigmoid distribution.
+      If ``hard=True``, the returned samples are descretized according to `threshold`, otherwise they will
+      be probability distributions.
+
+    """
+    gumbels = (
+        -torch.empty_like(logits, memory_format=torch.legacy_contiguous_format).exponential_().log()
+    )  # ~Gumbel(0, 1)
+    gumbels = (logits + gumbels) / tau  # ~Gumbel(logits, tau)
+    y_soft = gumbels.sigmoid()
+
+    if hard:
+        # Straight through.
+        indices = (y_soft > threshold).nonzero(as_tuple=True)
+        y_hard = torch.zeros_like(logits, memory_format=torch.legacy_contiguous_format)
+        y_hard[indices[0], indices[1]] = 1.0
+        ret = y_hard - y_soft.detach() + y_soft
+    else:
+        # Reparametrization trick.
+        ret = y_soft
+    return ret
+
